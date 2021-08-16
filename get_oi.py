c7d6471dc19deca4dc888ee895734307bf468010
kunmosky1 <kiyoshi@gumbo-star.sakura.ne.jp>
2021-08-17 00:13:44 +0900

    Update

--- get_oi.py
+++ get_oi.py
@@ -5,9 +5,10 @@ from libs import auth_db, oi_db
 
 if __name__ == '__main__':
 
-    host='oi.kumo.tokyo'
-    username = "123456789012345678"                            # DiscordIDを指定します
-    password = "abcdefghijklmnopqrstuvqxyz1234567890ABCDEFG="  # 管理人から提供されたパスワードを指定します
+    # これはサンプルです
+    host='hoge.hoge.jp'                                        # 管理人から指定されたホストを記載します
+    username = "123456789012345678"                            # DiscordIDを記載します
+    password = "abcdefghijklmnopqrstuvqxyz1234567890ABCDEFG="  # 管理人から提供されたパスワードを記載します
 
     # アクセス用のワンタイムパスワード生成（１日間有効）
     key = auth_db( host ).generate_key( username, password )
--- libs/__init__.py
+++ libs/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 #!/usr/bin/python3
 
-__all__ = ['auth_db', 'oi_db'']
+__all__ = ['auth_db', 'oi_db']
 
 from .database import auth_db
 from .oi import oi_db
