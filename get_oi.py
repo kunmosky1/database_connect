1fe6a04c2a9237fc7d441f0addc3fc543bec4677
kunmosky1 <kiyoshi@gumbo-star.sakura.ne.jp>
2021-08-17 16:18:03 +0900

    実際にお試しいただける username と password を記載しました

--- get_oi.py
+++ get_oi.py
@@ -5,10 +5,9 @@ from libs import auth_db, oi_db
 
 if __name__ == '__main__':
 
-    # これはサンプルです
-    host='hoge.hoge.jp'                                        # 管理人から指定されたホストを記載します
-    username = "123456789012345678"                            # DiscordIDを記載します
-    password = "abcdefghijklmnopqrstuvqxyz1234567890ABCDEFG="  # 管理人から提供されたパスワードを記載します
+    host='oi.kumo.tokyo'                                       # 管理人から指定されたホストを記載します
+    username = "999999999999999999"                            # DiscordIDを記載します
+    password = "ZiL6UW2SV4VHpy-oDnt9QLg5irldnbqdPWwzGbSM2Jg="  # 管理人から提供されたパスワードを記載します
 
     # アクセス用のワンタイムパスワード生成（6時間有効）
     key = auth_db(host).generate_key(username, password)
@@ -17,7 +16,7 @@ if __name__ == '__main__':
     db = oi_db(host, key)
 
     # ローソク足を取得 pediodは=30m, 4h, 3d, 1w などの取得期間を指定
-    print(db.query_oi(exchange='ftx', period="31d"))
+    print(db.query_oi(exchange='bybit', period="31d"))
 
     # 取得可能な取引所のリスト　['binance', 'bitmex', 'bybit', 'ftx', 'phemex']
     print(db.query_exchanges())
